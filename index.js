352bc343c752ad4ef0c9c985a770106fff3845a0
aletson <me@ajl.io>
2024-11-26 13:22:15 -0500

    ideally, this should work?

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ client.on('interactionCreate', async (interaction) => {
   if (interaction.isCommand()) {
     let did = interaction.options.getString('did');
     let handle = interaction.options.getString('handle').toLowerCase();
-    let { thisHandleExists } = await agent.getProfile({ actor: `${handle}.${domain}` }); // check if handle exists in Bluesky
+    try {
+        let thisHandleExists = await agent.getProfile({ actor: `${handle}.${domain}` }); // check if handle exists in Bluesky
     if (!thisHandleExists) { //handle does not exist in bluesky
       let pb_body = {
         apikey: process.env.pb_apikey,
@@ -85,5 +86,52 @@ client.on('interactionCreate', async (interaction) => {
     } else { // handle exists in Bluesky
       await interaction.reply({ content: 'Someone has already taken this custom subdomain, sorry', ephemeral: true });
     }
+  } catch (e) {
+    if (e instanceof XRPCError && e.message == 'Profile not found') { // handle does not exist
+        let pb_body = {
+            apikey: process.env.pb_apikey,
+            secretapikey: process.env.pb_secretkey
+          }
+          const response = await fetch(`https://porkbun.com/api/json/v3/dns/retrieveByNameType/${domain}/TXT/_atproto.${handle}`, { // check if subdomain exists
+            method: 'post',
+            body: JSON.stringify(pb_body),
+            headers: { 'Content-Type': 'application/json' }
+          });
+          let exists = response.json();
+          if (exists.records.length > 0) { // if subdomain exists, edit, because handle isn't valid right now in Bluesky. potential issue here where someone request a handle, doesn't verify, then someone else requests the same handle
+            let pb_body = {
+              apikey: process.env.pb_apikey,
+              secretapikey: process.env.pb_secretkey,
+              name: `_atproto.${handle}`,
+              type: "TXT",
+              content: did,
+              ttl: 600
+            };
+            const response = await fetch(`https://porkbun.com/api/json/v3/dns/edit/${domain}/${thisHandle[0][0].porkbun_id}`, {
+              method: 'post',
+              body: JSON.stringify(pb_body),
+              headers: { 'Content-Type': 'application/json' }
+            });
+            const data = await response.json();
+            await interaction.reply('New handle is set up.');
+          } else {
+            let pb_body = {
+              apikey: process.env.pb_apikey,
+              secretapikey: process.env.pb_secretkey,
+              name: `_atproto.${handle}`,
+              type: "CNAME",
+              content: domain,
+              ttl: 600
+            };
+            const response = await fetch(`https://porkbun.com/api/json/v3/dns/create/${domain}`, {
+              method: 'post',
+              body: JSON.stringify(pb_body),
+              headers: { 'Content-Type': 'application/json' }
+            });
+            const data = await response.json();
+            interaction.editReply({ content: 'Your handle should be set up at https://' + domain + ' in approximately five minutes.', ephemeral: true });
+          }
+    }
   }
+}
 });
